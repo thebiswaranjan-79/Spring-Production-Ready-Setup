2afab2136b30829a3cf47e62671e8ae2b53e55c2
Biswaranjan Mohanty <mbiswaranjan797@gmail.com>
2024-12-16 23:33:19 +0530

    First Commit'

---
+++ src/main/java/com/spring/production_ready_setup/production_ready_setup/ProductionReadySetupApplication.java
@@ -0,0 +1,13 @@
+package com.spring.production_ready_setup.production_ready_setup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductionReadySetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductionReadySetupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/production_ready_setup/production_ready_setup/ProductionReadySetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.production_ready_setup.production_ready_setup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductionReadySetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
